2e7f5447ab2a623fab30bf8fffa71f448ba07475
Zaborstik <zaborstik@gmail.com>
2022-10-04 13:50:54 +0300

    betta 1.5

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,4 +1,65 @@
 package src;
 
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height+2][width+2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public void clear(){
+        matrix = new char[height+2][width+2];
+    }
+
+    public void print(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void setPoint(double x, double y, char c){
+        if (x<0 || y<0 || y>=matrix.length || x>=matrix[0].length) return;
+        matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j]!=0){
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
 }
